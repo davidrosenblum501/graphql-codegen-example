46316349da44521e998374756972d5801b115d23
David Rosenblum <david.rosenblum@babycenter.com>
2023-02-08 10:19:19 -0500

    app styled component

--- app/src/components/jedis/JedisCard.tsx
+++ app/src/components/jedis/JedisCard.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 import Link from 'next/link';
 import { GetJediPaginatedQuery } from '../../codegen-server';
@@ -6,11 +7,15 @@ export interface JedisCardProps {
   jedi: GetJediPaginatedQuery['jediPaginated']['jedi'][0];
 }
 
+const StyledCard = styled(Card)`
+  margin-bottom: 15px;
+`;
+
 const JedisCard: React.FC<JedisCardProps> = ({
   jedi,
 }) => {
   return (
-    <Card variant="outlined" style={{ marginBottom: 15 }}>
+    <StyledCard variant="outlined">
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {jedi.name}
@@ -24,7 +29,7 @@ const JedisCard: React.FC<JedisCardProps> = ({
             <Button>See profile</Button>
           </Link>
       </CardActions>
-    </Card>
+    </StyledCard>
   );
 };
 
